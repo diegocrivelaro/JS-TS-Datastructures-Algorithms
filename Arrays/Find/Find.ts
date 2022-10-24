312991e60bc3bdf9c87597f3840b26711c9549fe
diegocrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2022-10-24 07:18:47 -0300

    feat: add Find studies JavaScript

---
+++ Arrays/Find/Find.ts
@@ -0,0 +1,15 @@
+const alunos = [
+  { nome: "João", nota: 7.3, bolsista: false },
+  { nome: "Diego", nota: 9.3, bolsista: false },
+  { nome: "Marta", nota: 4.6, bolsista: true },
+  { nome: "Suze", nota: 8.6, bolsista: true },
+  { nome: "Ricardo", nota: 2.6, bolsista: true },
+];
+
+const hasBolsista = alunos.find((item) => item.bolsista);
+const hasNameDiego = alunos.find((item) => item.nome === "Diego");
+const hasNameMariaLuisa = alunos.find((item) => item.nome === "Maria Luisa");
+
+console.log(hasBolsista);
+console.log(hasNameDiego);
+console.log(hasNameMariaLuisa);
