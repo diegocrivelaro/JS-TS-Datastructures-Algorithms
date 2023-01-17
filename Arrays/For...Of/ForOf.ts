a56b478fc9566cda6eb1f81f40d8cfd325000d93
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-17 06:44:48 -0300

    chore: complement For...Of studies

--- Arrays/For...Of/ForOf.ts
+++ Arrays/For...Of/ForOf.ts
@@ -1,5 +1,11 @@
 const cores = ["Vermelho", "Verde", "Azul"];
 
+// Forma antiga
+for (let indice = 0; indice < cores.length; indice++) {
+  console.log(cores[indice]);
+}
+
+// Forma nova
 for (const cor of cores) {
   console.log(cor);
 }
