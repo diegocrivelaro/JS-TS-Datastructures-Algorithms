152162e666acaa19880ed11297a6ddc6923b5d90
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-19 07:16:32 -0300

    feat: add Values studies JavaScript

---
+++ Values/Values.ts
@@ -0,0 +1,27 @@
+const letras = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+];
+
+console.log(Object.values(letras));
+// ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+
+const usuario = {
+  nome: "Diego",
+  sobrenome: "Ferreira",
+  idade: 25,
+};
+
+console.log(Object.values(usuario));
+// ["Diego", "Ferreira", 25];
