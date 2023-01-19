3263852e43b789322706fac726f64386b2e70b15
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-19 07:07:31 -0300

    feat: add Keys studies JavaScript

---
+++ Arrays/Keys/Keys.ts
@@ -0,0 +1,13 @@
+const usuario = {
+  nome: "João",
+  idade: 25,
+  email: "test@example.com",
+};
+
+console.log(Object.keys(usuario));
+// [ 'nome', 'idade', 'email' ]
+
+const letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+
+console.log(Object.keys(letras));
+// ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
