33f422335bcc00d252fdf2482ddf46417457cf64
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-24 07:21:10 -0300

    chore: complement Fill studies

--- Fill/Fill.ts
+++ Fill/Fill.ts
@@ -1,22 +1,29 @@
-const notas1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const notas2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const notas3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const notas4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const notas5 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const notas6 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Criar um array já preenchido com seus valores
+const array1 = Array(6).fill(2);
+console.log(array1);
+// [ 2, 2, 2, 2, 2, 2 ]
 
-const notasIguais1 = notas1.fill(50, 0, 5); // [50, 50, 50, 50, 5, 6, 7, 8, 9, 10]
-const notasIguais2 = notas2.fill(50, 0, 0); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const notasIguais3 = notas3.fill(50, 2, 2); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const notasIguais4 = notas4.fill(50, 2, 3); // [0, 1, 50, 3, 4, 5, 6, 7, 8, 9, 10]
-const notasIguais5 = notas5.fill(50, -11, -2); // [50, 50, 50, 50, 50, 50, 50, 50, 50, 9, 10]
+// Preencher todos valores do array com um único valor
+const array2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array2.fill(50));
+// [50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50];
 
-// Tal situação funciona, porém o TypeScript reclama, pois esse método espera um number
-const notasIguais6 = notas6.fill("Diego", 2, 8); // [0, 1, "Diego", "Diego","Diego", "Diego", "Diego", "Diego", 8, 9, 10]
+// Preencher com um valor a partir do índice 6
+const array3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array3.fill(50, 6));
+// [0, 1, 2, 3, 4, 5, 50, 50, 50, 50, 50];
 
-console.log(notasIguais1);
-console.log(notasIguais2);
-console.log(notasIguais3);
-console.log(notasIguais4);
-console.log(notasIguais5);
-console.log(notasIguais6);
+// Preencher com um valor a partir do índice 6 até o índice 8 (Excluindo o 8)
+const array4 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array4.fill(50, 6, 8));
+// [0, 1, 2, 3, 4, 5, 50, 50, 8, 9, 10];
+
+// Preencher com um valor a partir do índice negátivo (Irá andar de trás para frente, começando pelo -1, que é o último índice do array, e preenchendo até o índice -2, que é o penúltimo índice do array)
+const array5 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array5.fill(50, -2));
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 50, 50];
+
+// Preencher com um valor a partir do índice negativo até o índice negativo. (Irá andar de trás para frente. Começando do índice -11, que é o primeiro índice do array, e preenchendo até o índice -4, que é o sexto índice do array)
+const array6 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array6.fill(50, -11, -4));
+// [50, 50, 50, 50, 50, 50, 7, 8, 9, 10];
