47e0faaf456dd67edccff138806c57691c4c2806
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-01-31 07:27:07 -0300

    fix: fix logic

--- CopyWithin/CopyWithin.ts
+++ CopyWithin/CopyWithin.ts
@@ -18,6 +18,6 @@ console.log(numeros1.copyWithin(-3, 5, 8));
 // [1, 2, 3, 4, 5, 6, 7, 6, 7, 8];
 
 // Aceita valores negativos no parâmetro de índice inicial
-const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(numeros1.copyWithin(1, -2));
+const numeros2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numeros2.copyWithin(1, -2));
 // [1, 7, 8, 4, 5, 6, 7, 6, 7, 8];
