b05243de2a1eac5163618d2c83fb6799966835d2
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-02-01 07:18:06 -0300

    feat: add reverse studies javascript

---
+++ Reverse/Reverse.ts
@@ -0,0 +1,13 @@
+/*
+O método Reverse serve para inverter a ordem do array, ou seja, o último será o primeiro.
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+const reverseNumbers = numbers.reverse();
+console.log(reverseNumbers);
+// [18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+const names = ["João", "Maria", "José", "Antônio", "Mariana", "José"];
+const reverseNames = names.reverse();
+console.log(reverseNames);
+// ["José", "Mariana", "Antônio", "José", "Maria", "João"];
