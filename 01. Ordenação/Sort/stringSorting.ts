0317f88b86f4951c8c855bf2903fafc724eef2fb
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-05-22 07:50:23 -0300

    feat: add a string ordering test

---
+++ 01. Ordenação/Sort/stringSorting.ts
@@ -0,0 +1,17 @@
+const names = ["Ana", "ana", "john", "John"];
+
+// Ordenação padrão (Seguindo a tabela ASCII)
+console.log(names.sort());
+// [ 'Ana', 'John', 'ana', 'john' ]
+
+// Ordenação personalizada
+const lowerCaseCompareFunction = (a, b) => {
+  if (a.toLowerCase() < b.toLowerCase()) return -1;
+
+  if (a.toLowerCase() > b.toLowerCase()) return 1;
+
+  return 0;
+};
+
+console.log(names.sort(lowerCaseCompareFunction));
+// ["Ana", "ana", "John", "john"];
