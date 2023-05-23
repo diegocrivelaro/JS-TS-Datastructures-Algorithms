9e1255ed95836fc8753bf1ed632cb31e9933ebc7
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-05-23 07:39:12 -0300

    feat: add a new case of localeCompare with numbers

--- 03. localeCompare/testing.ts
+++ 03. localeCompare/testing.ts
@@ -1,19 +1,27 @@
-const letterA = "a"
-const letterB = "b"
+const letterA = "a";
+const letterB = "b";
 
-console.log(letterA.localeCompare(letterB))
+console.log(letterA.localeCompare(letterB));
 // -1 (A letra A vem antes da letra B, então o valor é negativo)
 
-console.log(letterB.localeCompare(letterA))
+console.log(letterB.localeCompare(letterA));
 // 1 (A letra B vem depois da letra A, então o valor é positivo)
 
-console.log(letterA.localeCompare(letterA))
+console.log(letterA.localeCompare(letterA));
 // 0 (A letra A é igual a letra A, então o valor é 0)
 
-const letterAWitchAccents = "à"
-const letterCWitchAccents = "ç"
+const letterAWitchAccents = "à";
+const letterCWitchAccents = "ç";
 
-console.log(letterAWitchAccents.localeCompare(letterCWitchAccents, "pt", {
-  ignorePunctuation: true,
-}))
+console.log(
+  letterAWitchAccents.localeCompare(letterCWitchAccents, "pt", {
+    ignorePunctuation: true,
+  })
+);
 // -1 (A letra A vem antes da letra C, então o valor é negativo)
+
+const number1 = "1";
+const number2 = "2";
+
+console.log(number1.localeCompare(number2, undefined, { numeric: true }));
+// -1 (O número 1 vem antes do número 2, então o valor é negativo)
