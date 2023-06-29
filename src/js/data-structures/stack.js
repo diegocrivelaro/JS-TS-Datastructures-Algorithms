5f0473d4067e728431eece36ea86f56628668319
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-29 07:51:07 -0300

    perf: change the Stack's internal structure for an object

--- src/js/data-structures/stack.js
+++ src/js/data-structures/stack.js
@@ -1,43 +1,66 @@
 // @ts-check
 
 /*
+isEmpty -> Retorna true se a pilha estiver vazia, ou false se o tamanho da pilha for maior que 0;
 push -> Adicionar um  ou vários elementos ao topo da pilha;
 pop -> Remove o elemento do topo da pilha, além de retornar o elemento removido;
 peek -> Retorna o elemento do topo da pilha, sem removê-lo;
-isEmpty -> Retorna true se a pilha estiver vazia, ou false se o tamanho da pilha for maior que 0;
 clear -> Limpar a pilha por completo;
 size -> Retorna o tamanho da pilha;
+list -> Retorna a estrutura stack;
 */
 
 class Stack {
   #items;
 
+  #count;
+
   constructor() {
-    this.#items = [];
-  }
-
-  push(element) {
-    this.#items.push(element);
-  }
-
-  pop() {
-    return this.#items.pop();
-  }
-
-  peek() {
-    return this.#items[this.#items.length - 1];
+    this.#items = {};
+    this.#count = 0;
   }
 
   isEmpty() {
-    return this.#items.length === 0;
+    return this.#count === 0;
+  }
+
+  push(element) {
+    this.#items[this.#count] = element;
+    this.#count++;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    this.#count--;
+    const result = this.#items[this.#count];
+    delete this.#items[this.#count];
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.#items[this.#count - 1];
   }
 
   clear() {
-    this.#items = [];
+    if (this.isEmpty()) {
+      return 0;
+    }
+
+    this.#items = {};
+    this.#count = 0;
+
+    return 1;
   }
 
   size() {
-    return this.#items.length;
+    return this.#count;
   }
 
   list() {
@@ -47,22 +70,5 @@ class Stack {
 
 const stack = new Stack();
 
-console.log(`stack.isEmpty(): ${stack.isEmpty()}`);
-
-stack.push(0);
 stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.push(4);
-console.log(`stack.isEmpty(): ${stack.isEmpty()}`);
-
-console.log(`stack.pop(): ${stack.pop()}`);
-
-console.log(`stack.peek(): ${stack.peek()}`);
-
-console.log(`stack.size(): ${stack.size()}`);
-
-console.log(`stack.list(): ${stack.list()}`);
-
-stack.clear();
-console.log(`stack.clear(): ${stack.list()}`);
+stack.list();
