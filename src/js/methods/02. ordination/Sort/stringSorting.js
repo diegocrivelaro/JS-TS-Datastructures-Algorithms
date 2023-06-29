bf8a6e8c6c6be5da7d7918d1a62c881920eb21a2
Diego Crivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2023-06-29 09:01:22 -0300

    refactor: organize folder structure

--- src/js/methods/02. ordination/Sort/stringSorting.js
+++ src/js/methods/02. ordination/Sort/stringSorting.js
@@ -1,13 +1,13 @@
 // @ts-check
 
-const names = ["Ana", "ana", "john", "John"];
+const names = ['Ana', 'ana', 'john', 'John'];
 
 // Ordenação padrão (Seguindo a tabela ASCII)
 console.log(names.sort());
 // [ 'Ana', 'John', 'ana', 'john' ]
 
 // Ordenação personalizada
-const lowerCaseCompareFunction = (a, b) => {
+const LowerCaseCompareFunction = (a, b) => {
   if (a.toLowerCase() < b.toLowerCase()) return -1;
 
   if (a.toLowerCase() > b.toLowerCase()) return 1;
@@ -15,5 +15,5 @@ const lowerCaseCompareFunction = (a, b) => {
   return 0;
 };
 
-console.log(names.sort(lowerCaseCompareFunction));
+console.log(names.sort(LowerCaseCompareFunction));
 // ["Ana", "ana", "John", "john"];
---
+++ src/js/methods/03. search/04. findIndex/FindIndex.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+const alunos = [
+  { nome: 'João', nota: 7.3, bolsista: false },
+  { nome: 'Diego', nota: 9.3, bolsista: false },
+  { nome: 'Marta', nota: 4.6, bolsista: true },
+  { nome: 'Suze', nota: 8.6, bolsista: true },
+  { nome: 'Ricardo', nota: 2.6, bolsista: true },
+];
+
+const index1 = alunos.findIndex((item) => item.nome.includes('Suze')); // 3
+const index2 = alunos.findIndex((item) => item.nota >= 7.6); // 1
+const index3 = alunos.findIndex((item) => item.nota < 1.6); // -1
+
+console.log(index1);
+console.log(index2);
+console.log(index3);
